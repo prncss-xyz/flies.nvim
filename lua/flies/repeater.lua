815a9abaf12677e386606a1711088140d1d2ba2d
Juliette Lamarche <juliette.lamarche@princesse.xyz>
2022-04-14 20:10:35 -0400

    added repeater module

---
+++ lua/flies/repeater.lua
@@ -0,0 +1,55 @@
+local M = {}
+
+local G = _G.Flies or {}
+_G.Flies = G
+
+local t = require('flies.utils').t
+
+local state
+local will_repeat
+local index
+local saved_state
+
+function M.querier(cb)
+  local res = M.querier_step(cb)
+  M.querier_save()
+  return res
+end
+
+function M.querier_step(cb)
+  index = index + 1
+  if will_repeat then
+    return saved_state[index]
+  end
+  state[index] = cb()
+  return state[index]
+end
+
+function M.querier_save()
+  if not will_repeat then
+    saved_state = state
+    state = {}
+  end
+  index = 0
+end
+
+function G.pre_dot()
+  will_repeat = true
+  return '.' .. t ":lua require'flies.repeater'.post_dot()<cr>"
+end
+
+function M.post_dot()
+  will_repeat = false
+end
+
+function M.setup()
+  M.querier_save()
+  vim.api.nvim_set_keymap(
+    'n',
+    '.',
+    'v:lua.Flies.pre_dot()',
+    { expr = true, noremap = true }
+  )
+end
+
+return M
