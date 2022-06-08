da69c007ca3b8051a3be7bcb7539b5a55ff7abdd
Juliette Lamarche <juliette.lamarche@princesse.xyz>
2022-06-08 15:38:55 -0400

    feat: pragmatic variable segment

--- lua/flies/objects/subline/variable_segment.lua
+++ lua/flies/objects/subline/variable_segment.lua
@@ -7,20 +7,29 @@ local variable_segment_seek0_cb = M.any(
   M.lua_pattern '()%u+()$',
   M.lua_pattern '()%u+()(%W)',
   M.lua_pattern '()%u+()(%u%S)',
-  M.lua_pattern '()%u?%l+(_?)'
+  M.lua_pattern '()%u?[%l%d]+(_?)'
 )
 
-function M.seek_cb(line, init)
+local function seek_cb(line, init)
   local os, is, ie, oe = variable_segment_seek0_cb(line, init)
   if not os then
     return
   end
   local c = string.sub(line, os - 1, os - 1)
-  local d = string.sub(line, oe + 1, oe + 1)
-  if (c == '_') and (d == '' or string.find(d, '[%A]')) then
+  local d = string.sub(line, oe, oe)
+  local e = string.sub(line, oe + 1, oe + 1)
+  if (c == '' or c:find '[^%w_]') and (e == '' or e:find '[^%w_]') then
+    return M.seek_cb(line, oe + 1)
+  end
+  if (c == '_') and (d ~= '_') and (e == '' or string.find(e, '[^%w_]')) then
     os = os - 1
   end
   return os, is, ie, oe
 end
+
+M.seek_cb = M.any(
+  seek_cb,
+  M.lua_pattern '()_+()'
+)
 
 return M
